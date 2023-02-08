d68401afe79fe3b320b6fb5216d468240e1aef6a
edudotdev <eduardo.er626@gmail.com>
2023-02-07 20:20:00 -0700

    feat: images providers

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['is1-ssl.mzstatic.com', 'is2-ssl.mzstatic.com', 'is3-ssl.mzstatic.com', 'is4-ssl.mzstatic.com', 'is5-ssl.mzstatic.com'],
+  },
 }
 
 module.exports = nextConfig
