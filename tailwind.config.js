e52008f8860ade9777fabba172287535707478c3
edudotdev <eduardo.er626@gmail.com>
2023-02-07 20:20:23 -0700

    feat: custom classes

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+ 
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      boxShadow: {
+        'before-link': '100px 0px 50px 100px rgb(58 130 246 / 25%)',
+        'before-nav': '-10px 120px 200px 200px rgb(58 130 246 / 25%)',
+        'player': '0px 0px 50px 50px rgb(0 0 0 / 25%)',
+      },
+      maxHeight: {
+        'content': 'calc(100vh - 152px)',
+      },
+      maxWidth: {
+        'player': 'calc(100% - 280px)' }
+    },
+  },
+  plugins: [],
+}
