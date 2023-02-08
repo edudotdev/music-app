c7da7397ddc186fa81103a59988d07de460f6b5b
edudotdev <eduardo.er626@gmail.com>
2023-02-07 20:35:36 -0700

    feat: like button component

---
+++ src/components/atoms/BtnLike/index.tsx
@@ -0,0 +1,51 @@
+import { Heart } from "phosphor-react";
+import { useEffect, useState } from "react";
+import localForage from "localforage";
+import { TRACK } from '@/types'
+
+
+interface BtnLikeProps {
+  className?: string
+  song: TRACK
+}
+
+export const BtnLike = ({
+  className = '',
+  song
+}:BtnLikeProps) => {
+  const [active, setActive] = useState(false)
+
+  const handleClick = async () => {
+    setActive(!active)
+
+    if (await localForage.getItem('likes') === null) {
+      localForage.setItem('likes', []);
+    }
+
+    if (!active) {
+      localForage.getItem('likes')
+        .then((result:any) => {
+          localForage.setItem('likes', [song, ...result])
+        })
+    } else {
+      localForage.getItem('likes')
+        .then((result:any) => {
+          result = result.filter((items: TRACK) => items.id !== song.id)
+          localForage.setItem('likes', result)
+        })
+    }
+  }
+  useEffect(() => {
+    localForage.getItem('likes')
+      .then((result:any) => {
+        result.find((item:TRACK) => item.id === song.id) && setActive(true)
+      })
+  }, [song])
+  
+
+  return (
+    <button onClick={handleClick} className={`p-2 ${active? 'grayscale-0' : 'grayscale'} ${className}`}>
+      <Heart size={25} color="#ef4444" weight="fill" className='' />
+    </button>
+  )
+}
