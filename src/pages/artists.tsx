ae9d21d030c100727630c482c3d83760550aae94
edudotdev <eduardo.er626@gmail.com>
2023-02-07 20:32:35 -0700

    default ui pages

---
+++ src/pages/artists.tsx
@@ -0,0 +1,9 @@
+import { Layout } from '@/components/Layout'
+
+export default function Artists () {
+  return (
+    <Layout title='Artists'>
+      <h2 className='text-neutral-100 font-bold text-4xl'>Artists</h2>
+    </Layout>
+  )
+}
---
+++ src/pages/for-you.tsx
@@ -0,0 +1,11 @@
+import { Layout } from '@/components/Layout'
+import React from 'react'
+
+export default function ForYou () {
+  return (
+    <Layout title='For You'>
+      <h2 className='text-neutral-100 font-bold text-4xl'>For You</h2>
+    </Layout>
+  )
+}
+
