19349d017c197b1fae13dd72813d2db4521f90f7
edudotdev <eduardo.er626@gmail.com>
2023-02-07 20:36:22 -0700

    feat: searcher input component

---
+++ src/components/molecules/Search/index.tsx
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+import { MagnifyingGlass } from "phosphor-react";
+import { usePlayerStore } from '@/store/playerStore'
+import router from 'next/router';
+
+export const Search = () => {
+
+  const [inputValue, setInputValue] = useState('')
+  const {track, setTrack} = usePlayerStore()
+
+  const handleChange = (e: any) => {
+    setInputValue(e.target.value)
+  }
+
+  const handleSubmit = async (e:any) => {
+    e.preventDefault()
+    
+    router.push(`/search?song=${inputValue.toLocaleLowerCase().trim()}`)
+  }
+
+  return (
+    <form onSubmit={e => handleSubmit(e)} className='mx-4'>
+      <div className='bg-white/10 w-full flex gap-3 text-sm text-blue-200 p-4 rounded-2xl'>
+        <MagnifyingGlass size={28} color="#fff" weight="fill" />
+        <input 
+          className='bg-transparent outline-none text-gray-200 w-full'
+          type="text" value={inputValue} onChange={e => handleChange(e)} placeholder='search music'   
+        />
+      </div>
+    </form>
+  )
+}
