04a7e7e6b69040a5a09fbf107a22699c5e0f6e58
edudotdev <eduardo.er626@gmail.com>
2023-02-07 20:37:35 -0700

    feat: nav bar

---
+++ src/components/molecules/NavLink/index.tsx
@@ -0,0 +1,29 @@
+import { useRouter } from 'next/router'
+import Link from 'next/link'
+
+interface NavLinkProps {
+  children: React.ReactChild
+  to: string
+  text:string
+}
+
+export const NavLink = ({
+  children,
+  to,
+  text
+}:NavLinkProps) => {
+  const router = useRouter()
+
+  const styleDefault = 'font-semibold pl-6 py-5 flex gap-4 items-center hover:bg-neutral-600/20'
+  const styles = 'text-gray-300'
+  const styleActive = 'relative text-blue-200 overflow-hidden before:absolute before:w-1.5 before:h-9 before:right-0 before:rounded-l-lg before:top-1/2 before:-translate-y-1/2 before:bg-blue-500'
+
+  return (
+    <Link 
+      className={`${styleDefault} ${router.asPath === to? styleActive : styles }`} 
+      href={to}>
+        {children}
+        <span>{text}</span>
+    </Link>
+  )
+}
---
+++ src/components/organisms/Nav/index.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import { Logo } from '../../atoms/'
+import { Search, NavLink } from '../../molecules/'
+import { GlobeSimple, Disc, MicrophoneStage, UsersThree, Heart } from "phosphor-react";
+
+export const Nav = () => {
+
+  const colorIcon: string = '#dbeafe'
+
+  const sizeIcon: number = 28
+ 
+  return (
+    <div className='flex flex-col gap-10 py-10 bg-neutral-800 bg-opacity-50 min-w-[260px] w-[260px] h-screen sticky top-0 before:block before:absolute before:w-5 before:h-5 before:rounded-full before:left-0 before:-top-10 before:bg-blue-500 before:shadow-before-nav'>
+      <Logo />
+      <Search />
+      <div className='flex flex-col'>
+        <h2 className='text-neutral-400 ml-6 pb-3 text-sm'>Explore</h2>
+        <NavLink to='/' text='Discover'>
+          <GlobeSimple size={sizeIcon} color={colorIcon} weight="fill" />
+        </NavLink>
+        <NavLink to='/for-you' text='For you'>
+          <Disc size={sizeIcon} color={colorIcon} weight="fill" />
+        </NavLink>
+      </div>
+      <div className='flex flex-col'>
+        <h2 className='text-neutral-400 ml-6 pb-3 text-sm'>Trending</h2>
+        <NavLink to='/artists' text='Artists'>
+          <UsersThree size={sizeIcon} color={colorIcon} weight="fill" />
+        </NavLink>
+        <NavLink to='/songs' text='Songs'>
+          <MicrophoneStage size={32} color={colorIcon} weight="fill" />
+        </NavLink>
+      </div>
+      <div className='flex flex-col'>
+        <h2 className='text-neutral-400 ml-6 pb-3 text-sm'>Your collection</h2>
+        <NavLink to='/likes' text='Likes'>
+          <Heart size={sizeIcon} color={colorIcon} weight="fill" />
+        </NavLink>
+      </div>
+    </div>
+  )
+}
