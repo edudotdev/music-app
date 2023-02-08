7344e912a51a5bea3c42ca7d3c39262c2607b120
edudotdev <eduardo.er626@gmail.com>
2023-02-07 20:30:34 -0700

    feat: set background

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,11 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import { PlayerMusic } from '@/components/organisms'
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body>
+      <body className='bg-neutral-900'>
         <Main />
         <NextScript />
       </body>
