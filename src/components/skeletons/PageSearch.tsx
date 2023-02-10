27e7335d7e46c195be1ff056783a685aa4e4a8de
edudotdev <eduardo.er626@gmail.com>
2023-02-10 01:10:27 -0700

    search skeleton

---
+++ src/components/skeletons/PageSearch.tsx
@@ -0,0 +1,20 @@
+
+export const PageSearch = () => {
+
+  return (
+    <div className="mx-auto w-full max-w-screen-2xl flex flex-col gap-10"> 
+      <div className="h-12 w-52 bg-neutral-700 animate-pulse"></div>
+      <div className='grid gap-10 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4'>
+        {Array(5).fill('').map((i:number, index:number) => (
+          <div key={index} className='relative w-auto rounded-xl overflow-hidden'>
+            <div className='w-[400px] h-auto max-w-full aspect-square bg-neutral-600 animate-pulse'></div>
+            <div className='h-20 p-4 w-full flex flex-col justify-between bg-neutral-800 '>
+              <p className='h-4 lg:h-5 w-24 bg-neutral-700 animate-pulse'></p>
+              <p className='h-4 lg:h-5 w-16 bg-neutral-700 animate-pulse'></p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
