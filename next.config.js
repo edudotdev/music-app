6755c9fc9bf11ac2977891265977cc705567afc8
edudotdev <eduardo.er626@gmail.com>
2023-02-10 00:48:16 -0700

    support appDir

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    appDir: true
+  },
   reactStrictMode: true,
   images: {
     domains: ['is1-ssl.mzstatic.com', 'is2-ssl.mzstatic.com', 'is3-ssl.mzstatic.com', 'is4-ssl.mzstatic.com', 'is5-ssl.mzstatic.com'],
