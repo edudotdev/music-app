ac11a551df00d4b532efa4d67db90d8cc19c3801
edudotdev <eduardo.er626@gmail.com>
2023-02-11 01:25:58 -0700

    some styles

--- src/pages/likes.tsx
+++ src/pages/likes.tsx
@@ -3,6 +3,7 @@ import localForage from 'localforage'
 import { useEffect, useState } from 'react'
 import { CardSong } from '@/components/molecules'
 import { TRACK } from '@/types'
+import { Heart} from 'phosphor-react'
 
 export default function Likes () {
   const [favorites, setFavorites] = useState<TRACK[]>([])
@@ -13,9 +14,9 @@ export default function Likes () {
   
   return (
     <Layout title='Likes'>
-      <h2 className='text-neutral-100 font-bold text-4xl'>Likes - {favorites.length}</h2>
-      <div className='grid gap-10 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4'>
-        {favorites.map((song: TRACK) => (( 
+      <h2 className='text-neutral-100 font-bold text-4xl'>Likes - <span className='text-blue-300'>{Boolean(favorites)? favorites.length : 0}</span></h2>
+      <div className='grid gap-10 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4'>
+        {favorites?.map((song: TRACK) => (( 
           <CardSong key={song.id} song={song} />
         )))}
       </div>
