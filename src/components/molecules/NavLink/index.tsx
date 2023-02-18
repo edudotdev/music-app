1c0acfc69395415484e2b916551a44f323fbd294
edudotdev <eduardo.er626@gmail.com>
2023-02-17 19:50:19 -0700

    fix: detect route

--- src/components/molecules/NavLink/index.tsx
+++ src/components/molecules/NavLink/index.tsx
@@ -1,4 +1,4 @@
-import { useRouter } from 'next/navigation'
+import { useRouter } from 'next/router'
 import Link from 'next/link'
 
 interface NavLinkProps {
@@ -14,14 +14,13 @@ export const NavLink = ({
 }:NavLinkProps) => {
   const router = useRouter()
   
-  const styleDefault = 'font-semibold pl-6 py-5 flex gap-4 items-center hover:bg-neutral-600/20'
+  const styleDefault = 'font-semibold pl-6 py-4 flex gap-4 items-center hover:bg-neutral-600/20'
   const styles = 'text-gray-300'
   const styleActive = 'relative text-blue-200 overflow-hidden before:absolute before:w-1.5 before:h-9 before:right-0 before:rounded-l-lg before:top-1/2 before:-translate-y-1/2 before:bg-blue-500'
 
   return (
     <Link 
-    //no jala con las nuevas rutas ${router.asPath === to? styleActive : styles }
-      className={`${styleDefault} ${styles}`} 
+      className={`${styleDefault} ${styles} ${router.asPath === to? styleActive : styles }`} 
       href={to}>
         {children}
         <span>{text}</span>
