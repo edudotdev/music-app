b41492c2151f9373e93309db1f110a369850c09d
edudotdev <eduardo.er626@gmail.com>
2023-02-17 19:49:58 -0700

    feat: reduce paddings

--- src/components/organisms/Nav/index.tsx
+++ src/components/organisms/Nav/index.tsx
@@ -1,22 +1,19 @@
-'use client';
-
 import React from 'react'
 import { Logo } from '../../atoms/'
 import { Search, NavLink } from '../../molecules/'
-import { GlobeSimple, Disc, MicrophoneStage, UsersThree, Heart } from "phosphor-react";
+import { GlobeSimple, Disc, MicrophoneStage, UsersThree, Heart, Playlist } from "phosphor-react";
 
 export const Nav = () => {
 
   const colorIcon: string = '#dbeafe'
-
   const sizeIcon: number = 28
  
   return (
-    <div className='flex flex-col gap-10 py-10 bg-neutral-800 bg-opacity-50 min-w-[260px] w-[260px] h-screen sticky top-0 before:block before:absolute before:w-5 before:h-5 before:rounded-full before:left-0 before:-top-10 before:bg-blue-500 before:shadow-before-nav lg:z-30'>
+    <div className='flex flex-col gap-8 py-10 bg-neutral-800 bg-opacity-50 min-w-[260px] w-[260px] h-screen sticky top-0 before:block before:absolute before:w-5 before:h-5 before:rounded-full before:left-0 before:-top-10 before:bg-blue-500 before:shadow-before-nav lg:z-30'>
       <Logo />
       <Search />
       <div className='flex flex-col'>
-        <h2 className='text-neutral-400 ml-6 pb-3 text-sm'>Explore</h2>
+        <h2 className='text-neutral-400 ml-6 pb-2 text-sm'>Explore</h2>
         <NavLink to='/' text='Discover'>
           <GlobeSimple size={sizeIcon} color={colorIcon} weight="fill" />
         </NavLink>
@@ -25,7 +22,7 @@ export const Nav = () => {
         </NavLink>
       </div>
       <div className='flex flex-col'>
-        <h2 className='text-neutral-400 ml-6 pb-3 text-sm'>Trending</h2>
+        <h2 className='text-neutral-400 ml-6 pb-2 text-sm'>Trending</h2>
         <NavLink to='/artists' text='Artists'>
           <UsersThree size={sizeIcon} color={colorIcon} weight="fill" />
         </NavLink>
@@ -34,9 +31,15 @@ export const Nav = () => {
         </NavLink>
       </div>
       <div className='flex flex-col'>
-        <h2 className='text-neutral-400 ml-6 pb-3 text-sm'>Your collection</h2>
+        <h2 className='text-neutral-400 ml-6 pb-2 text-sm'>Your collection</h2>
         <NavLink to='/likes' text='Likes'>
           <Heart size={sizeIcon} color={colorIcon} weight="fill" />
+        </NavLink>
+      </div>
+      <div className='flex flex-col'>
+        <h2 className='text-neutral-400 ml-6 pb-2 text-sm'>Your playlists</h2>
+        <NavLink to='/playlists' text='All playlists'>
+          <Playlist size={sizeIcon} color={colorIcon} weight="fill" />
         </NavLink>
       </div>
     </div>
