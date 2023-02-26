9075e348bb59b98bfe9d571bafbb645b51d53a0f
edudotdev <eduardo.er626@gmail.com>
2023-02-25 23:06:42 -0700

    feat: button play and shuffle in card playlist

--- src/components/atoms/BtnOptionsPlaylist/index.tsx
+++ src/components/atoms/BtnOptionsPlaylist/index.tsx
@@ -1,18 +1,35 @@
-import { DotsThreeOutline, PlayCircle } from 'phosphor-react'
-import React, { useState } from 'react'
+import { TRACK } from '@/types'
+import { DotsThreeOutline, PlayCircle, ShuffleAngular } from 'phosphor-react'
+import React, { useEffect, useState } from 'react'
+import { usePlayerIndexStore, usePlayerStore, } from '@/store/playerStore'
 
 interface BtnOptionsPlaylistProps {
   className?: string
   showMenu: boolean
   setShowMenu: (value: boolean) => void
-  
+  songs: TRACK[]
 }
 
 export const BtnOptionsPlaylist = ({
   className = '',
   showMenu,
-  setShowMenu
+  setShowMenu,
+  songs
 }:BtnOptionsPlaylistProps) => {
+  const {setTrack, tracks} = usePlayerStore()
+  const {setIndex} = usePlayerIndexStore()
+
+  const handlePlay = () => {
+    setTrack(songs)
+    setIndex(0)
+  }
+
+  const handleShuffle = () => {
+    const shuffleSongs = shuffle(songs)
+    
+    setTrack(shuffleSongs)
+    setIndex(0)
+  }
 
   return (
     <div className={`absolute group-hover:z-10 w-48 text-right ${className}`}>
@@ -23,11 +40,15 @@ export const BtnOptionsPlaylist = ({
           </button>
         </div>
         {showMenu && (
-          <div className="absolute right-0 bottom-[50px] mt-2 w-36 origin-bottom-right divide-y divide-gray-100 rounded-md bg-neutral-900/95 shadow-lg ring-1 ring-white ring-opacity-5 focus:outline-none">
+          <div className="absolute right-0 bottom-[50px] mt-2 w-36 origin-bottom-right rounded-md bg-neutral-900/95 shadow-lg ring-opacity-5 focus:outline-none">
             <div className="py-2.5">
-              <button className='text-blue-100 group flex gap-1.5 w-full items-center py-2 px-3 transition-[padding] font-semibold text-sm hover:bg-blue-300/20'>
+              <button onClick={handlePlay} className='text-blue-100 group flex gap-1.5 w-full items-center py-2 px-3 font-semibold text-sm hover:bg-blue-300/20'>
                 <PlayCircle size={17} color='#dbeafe' weight="fill" />
                 <span>Play</span>
+              </button>
+              <button onClick={handleShuffle} className='text-blue-100 group flex gap-1.5 w-full items-center py-2 px-3 font-semibold text-sm hover:bg-blue-300/20'>
+                <ShuffleAngular size={17} color='#dbeafe' weight="fill" />
+                <span>Shuffle</span>
               </button>
             </div>
           </div>
@@ -35,4 +56,9 @@ export const BtnOptionsPlaylist = ({
       </div>
     </div>
   )
+}
+
+
+const shuffle = (array: TRACK[]) => {
+  return array.sort(() => Math.random() - 0.5);
 }
