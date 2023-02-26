a7bab3413162440ea96636c89a2a59e67435cbcd
edudotdev <eduardo.er626@gmail.com>
2023-02-25 23:07:06 -0700

    reduced paddings

--- src/components/molecules/NavLink/index.tsx
+++ src/components/molecules/NavLink/index.tsx
@@ -16,7 +16,7 @@ export const NavLink = ({
   
   const styleDefault = 'font-semibold pl-6 py-4 flex gap-4 items-center hover:bg-neutral-600/20'
   const styles = 'text-gray-300'
-  const styleActive = 'relative text-blue-200 overflow-hidden before:absolute before:w-1.5 before:h-9 before:right-0 before:rounded-l-lg before:top-1/2 before:-translate-y-1/2 before:bg-blue-500'
+  const styleActive = 'relative text-blue-200 overflow-hidden before:absolute before:w-1 before:h-6 before:right-0 before:rounded-l-lg before:top-1/2 before:-translate-y-1/2 before:bg-blue-500'
 
   return (
     <Link 
