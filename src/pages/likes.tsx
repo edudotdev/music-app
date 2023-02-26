eec83250e7a18ec3b5104dcba409927375af0092
edudotdev <eduardo.er626@gmail.com>
2023-02-25 22:58:14 -0700

    feat: button play and shuffle

--- src/pages/likes.tsx
+++ src/pages/likes.tsx
@@ -3,18 +3,47 @@ import localForage from 'localforage'
 import { useEffect, useState } from 'react'
 import { CardSong } from '@/components/molecules'
 import { TRACK } from '@/types'
-import { Heart} from 'phosphor-react'
+import { Play, ShuffleAngular } from 'phosphor-react'
+import { usePlayerStore } from '@/store/playerStore'
 
 export default function Likes () {
   const [favorites, setFavorites] = useState<TRACK[]>([])
+  const {setTrack} = usePlayerStore()
+
 
   useEffect(() => {
-    handleFavorites(setFavorites)
+    handleFavorites()
   }, [])
+
+  const handlePlay = () => {
+    setTrack(favorites)
+  }
+
+  const handleFavorites = async () => {
+    await localForage.getItem('likes').then((result:any) => {
+      if (result === null) result = []
+      setFavorites(result)
+    })
+  }
   
   return (
     <Layout title='Likes'>
-      <h2 className='text-neutral-100 font-bold text-4xl'>Likes - <span className='text-blue-300'>{Boolean(favorites)? favorites.length : 0}</span></h2>
+      <div className='flex justify-between'>
+        <h2 className='text-neutral-100 flex gap-1.5 font-bold text-4xl'>
+          <span>Likes -</span>
+          <span className='text-blue-300'>{Boolean(favorites)? favorites.length : 0}</span>
+        </h2>
+        <div className='flex items-center gap-3'>
+          <button onClick={handlePlay} className='flex items-center justify-center gap-2 bg-blue-500 py-2 w-32 text-white text-sm font-semibold rounded-md'>
+            <Play size={16} color="#fff" weight="fill" />
+            play
+          </button>
+          <button className='flex items-center justify-center gap-2 bg-blue-500 py-2 w-32 text-white text-sm font-semibold rounded-md'>
+            <ShuffleAngular size={20} color="#fff" weight="fill" />
+            shuffle
+          </button>
+        </div>
+      </div>
       <div className='grid gap-10 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4'>
         {favorites?.map((song: TRACK) => (( 
           <CardSong key={song.id} song={song} />
@@ -22,9 +51,4 @@ export default function Likes () {
       </div>
     </Layout>
   )
-}
-
-const handleFavorites = async (setFavorites: any) => {
-  const likes = await localForage.getItem('likes')
-  setFavorites(likes)
 }
