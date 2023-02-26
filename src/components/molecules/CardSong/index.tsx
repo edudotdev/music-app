9e3fd6794e8ed225042e3db0c5e1614945ba5d86
edudotdev <eduardo.er626@gmail.com>
2023-02-25 23:05:27 -0700

    removed component and fix code

--- src/components/molecules/CardSong/index.tsx
+++ src/components/molecules/CardSong/index.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image'
-import { usePlayerStore } from '@/store/playerStore'
-import { BtnLike, BtnOptionsSong, AddedPlaylistInfo } from '@/components/atoms'
+import { usePlayerIndexStore, usePlayerStore } from '@/store/playerStore'
+import { BtnLike, BtnOptionsSong } from '@/components/atoms'
 import { useState } from 'react';
 import { TRACK } from '@/types'
 
@@ -13,23 +13,25 @@ export const CardSong = ({
 }:CardSongProps) => {
   const [text, setText] = useState('')
   const {setTrack} = usePlayerStore()
+  const {setIndex} = usePlayerIndexStore()
+
   const [showMenu, setShowMenu] = useState(false)
 
   const handlePlay = () => {
-    setTrack({
+    setIndex(0)
+    setTrack([{
       id,
       music,
       title,
       artist,
       image
-    })
+    }])
   }
 
   return (
     <div className='relative group' onMouseLeave={() => setShowMenu(false)}>
       <BtnLike song={{id,music,title, artist,image}} className='absolute group-hover:z-10 opacity-0 group-hover:opacity-100 right-2 top-2 transition-all'  />
-      <BtnOptionsSong song={{id,music,title, artist,image}} setShowMenu={setShowMenu} showMenu={showMenu} setText={setText} className='opacity-0 group-hover:opacity-100 transition-all' />
-      <AddedPlaylistInfo namePlaylist={text} className={`${Boolean(text.length)? 'group-hover:opacity-100 visible' : 'invisible opacity-0'}`} />
+      <BtnOptionsSong song={{id,music,title, artist,image}} setShowMenu={setShowMenu} showMenu={showMenu} className='opacity-0 group-hover:opacity-100 transition-all' />
       <div onClick={handlePlay} className='relative cursor-pointer rounded-xl overflow-hidden active:scale-[.99] transition-transform'>
         <Image src={image} width={400} height={400} className='w-full' alt={title} />
         <div className='h-20 relative overflow-hidden rounded-b-xl'>
