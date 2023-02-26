94d1a01cb5e5b1ada11b0ec4965d9919890b5e2b
edudotdev <eduardo.er626@gmail.com>
2023-02-25 23:03:30 -0700

    feat: show alert when song added to some playlist

--- src/components/molecules/ModalNewPlaylist/index.tsx
+++ src/components/molecules/ModalNewPlaylist/index.tsx
@@ -1,24 +1,21 @@
 import React, { useEffect, useState } from 'react';
-import { TRACK } from '@/types'
+import { TRACK, PLAYLIST } from '@/types'
 import localForage from 'localforage'
 import { newPlaylist, addSong } from '@/crud/playlist'
 import { Playlist } from 'phosphor-react';
+import { useActionInfoStore } from '@/store/actionInfoStore'
 
 interface ModalNewPlaylistProps {
-  className: string
   song: TRACK
-  setText: any
 }
 
 export const ModalNewPlaylist = ({
-  className,
   song,
-  setText
 }:ModalNewPlaylistProps) => {
   const [isOpen, setIsOpen] = useState(false);
   const [namePlaylist, setNamePlaylist] = useState('')
   const [playlists, setPlaylists] = useState([])
-  const [timer, setTimer] = useState<any>(null)
+  const { setTextInfo } = useActionInfoStore()
 
   const handleChange = (e: any) => {
     setNamePlaylist(e.target.value)
@@ -32,13 +29,20 @@ export const ModalNewPlaylist = ({
     newPlaylist(namePlaylist, song)
     setNamePlaylist('')
     setIsOpen(false)
+
+    setTextInfo({
+      text:'Added to playlist',
+      active: true
+    })
   }
 
-  const showPlaylistInfo = (text: string) => {
-    setText(text)
-    clearTimeout(timer)
-    const newTimer = setTimeout(() => {setText('')}, 2500)
-    setTimer(newTimer)
+  const handleClick = (playlist: PLAYLIST) => {
+    addSong(playlist.uuid, song)
+    setIsOpen(false)
+    setTextInfo({
+      text:'Added to playlist',
+      active: true
+    })
   }
 
   useEffect(() => {
@@ -54,11 +58,11 @@ export const ModalNewPlaylist = ({
       <div className='relative right-full -translate-x-5 -top-[46px]'>
         {isOpen && (
           <div className='absolute rounded-md bg-neutral-900/95 shadow-lg w-40 max-h-[210px] overflow-y-auto py-2.5 content'>
-            <form action="" onSubmit={(e) => {showPlaylistInfo(namePlaylist); handleSubmit(e)}}>
+            <form onSubmit={(e) => handleSubmit(e)}>
               <input type="text" onChange={(e) => handleChange(e)} value={namePlaylist} placeholder='add playlist...' className='w-full text-sm outline-none bg-transparent focus:bg-blue-300/20 border-transparent text-white pl-3 py-2' />
             </form>
-            {playlists.length > 0 && playlists.map((playlist:any) => (
-              <button key={playlist.uuid} onClick={() => {addSong(playlist.uuid, song); showPlaylistInfo(playlist.name); setIsOpen(false)}} className='cursor-pointer flex gap-2 w-full text-start hover:bg-blue-300/20 py-2 px-3 text-sm font-semibold text-blue-100' title={playlist.name}>
+            {playlists.length > 0 && playlists.map((playlist:PLAYLIST) => (
+              <button key={playlist.uuid} onClick={() => handleClick(playlist)} className='cursor-pointer flex gap-2 w-full text-start hover:bg-blue-300/20 py-2 px-3 text-sm font-semibold text-blue-100' title={playlist.name}>
                 <Playlist size={17} color="#dbeafe" weight="fill" /> <span className='truncate'>{playlist.name}</span>
               </button>
             ))}
