b0d9cd72d8bbd9b1225316aab4c0a99411340b50
edudotdev <eduardo.er626@gmail.com>
2023-02-25 22:55:34 -0700

    feat: add song to queue

---
+++ src/components/atoms/BtnAddQueue/index.tsx
@@ -0,0 +1,34 @@
+import { usePlayerStore } from '@/store/playerStore'
+import { TRACK } from '@/types'
+import { MusicNotesPlus } from 'phosphor-react'
+import React, { useState } from 'react'
+import { shallow } from 'zustand/shallow'
+import { useActionInfoStore } from '@/store/actionInfoStore'
+
+interface BtnAddQueueProps {
+  song: TRACK
+}
+
+export const BtnAddQueue = ({
+  song,
+}:BtnAddQueueProps) => {
+  const { setTextInfo } = useActionInfoStore()
+  const {tracks} = usePlayerStore((state) => ({
+    tracks: state.tracks
+  }), shallow)
+
+  const handleAddQueue = () => {
+    tracks.push(song)
+    setTextInfo({
+      text:'Added to queue',
+      active: true
+    })
+  }
+  
+  return (
+    <button onClick={handleAddQueue} className='text-blue-100 group flex gap-1.5 w-full items-center py-2 px-3 transition-[padding] font-semibold text-sm hover:bg-blue-300/20'>
+      <MusicNotesPlus size={17} color='#dbeafe' weight="fill" />
+      <span>Add queue</span>
+    </button>
+  )
+}
