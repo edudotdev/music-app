0ef82476b03b142dcf68f338a471a0e10d758065
edudotdev <eduardo.er626@gmail.com>
2023-02-25 23:06:02 -0700

    feat: factored code

--- src/components/molecules/CardPlaylist/index.tsx
+++ src/components/molecules/CardPlaylist/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
 import { TRACK, PLAYLIST } from '@/types'
@@ -12,11 +12,15 @@ export const CardPlaylist = ({
   playlist
 }:CardPlaylistProps) => {
   const [showMenu, setShowMenu] = useState(false)
+  const [songs, setSongs] = useState([])
 
+  useEffect(() => {
+    setSongs(playlist.song)
+  }, [playlist])
 
   return (
     <div key={playlist.uuid} className='relative flex flex-col w-auto aspect-square group' onMouseLeave={() => setShowMenu(false)}>
-      <BtnOptionsPlaylist showMenu={showMenu} setShowMenu={setShowMenu} className='bottom-12 right-2 opacity-0 group-hover:opacity-100 transition-opacity' />
+      <BtnOptionsPlaylist songs={songs} showMenu={showMenu} setShowMenu={setShowMenu} className='bottom-12 right-2 opacity-0 group-hover:opacity-100 transition-opacity' />
       <div className='h-full rounded-xl overflow-hidden border-2 border-neutral-700 border-opacity-70'>
         <Link href={`/playlist/${playlist.uuid}`} className='active:scale-[.99]'>
           <div className='grid grid-cols-2 h-full '>
