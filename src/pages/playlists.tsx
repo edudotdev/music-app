239c93f9c4f4c8d477adce834a18d2d37088b14e
edudotdev <eduardo.er626@gmail.com>
2023-02-27 22:43:24 -0700

    feat: added newPlaylist modal

--- src/pages/playlists.tsx
+++ src/pages/playlists.tsx
@@ -2,11 +2,12 @@ import { Layout } from '@/components/Layout'
 import { PlusCircle } from 'phosphor-react'
 import localForage from 'localforage'
 import { useEffect, useState } from 'react'
-import { CardPlaylist } from '@/components/molecules'
+import { CardPlaylist, ModalNewPlaylist } from '@/components/molecules'
 import { PLAYLIST } from '@/types'
 
 export default function Playlists () {
   const [playlists, setPlaylists] = useState([])
+  const [showModal, setShowModal] = useState(false)
 
   const handlePlaylists = async () => {
     await localForage.getItem('playlists')
@@ -18,22 +19,21 @@ export default function Playlists () {
 
   useEffect(() => {
     handlePlaylists()
-  }, [])
+    console.log('refresh')
+  }, [showModal])
 
   return (
     <Layout title='Playlists'>
+     <div className='flex justify-between'>
       <h2 className='text-neutral-100 font-bold text-4xl'>Playlists - <span className='text-blue-300'>{Boolean(playlists)? playlists.length : 0}</span></h2>
+      <button onClick={() => setShowModal(true)} className='flex items-center justify-center gap-2 bg-blue-500 py-2 w-32 text-white text-sm font-semibold rounded-md outline-none'>New Playlist</button>
+     </div>
       <div className='grid gap-10 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4'>
         {playlists.map((playlist:PLAYLIST) => (
           <CardPlaylist key={playlist.uuid} playlist={playlist} />
-          ))}
-        <button className='relative flex flex-col active:scale-[.99] w-auto'>
-          <div className='w-[400px] grid place-items-center h-auto max-w-full rounded-xl overflow-hidden aspect-square bg-neutral-700/60 border-2 border-neutral-600/70 border-opacity-70'>
-            <PlusCircle color='#ccc' size={110} weight="fill" className='opacity-40' />
-          </div>
-          <h2 className='text-white p-2'>New Playlist</h2>
-        </button>
+        ))}
       </div>
+      {showModal && <ModalNewPlaylist setShowModal={setShowModal} />}
     </Layout>
   )
 }
