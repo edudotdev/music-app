4dd98b9d5bb86358e559601a314f11a3d36eb7ee
edudotdev <eduardo.er626@gmail.com>
2023-03-03 23:39:31 -0700

    fix: colors

--- src/components/skeletons/SkeletonSearch.tsx
+++ src/components/skeletons/SkeletonSearch.tsx
@@ -6,9 +6,9 @@ export const SkeletonSearch = () => {
     {Array(5).fill('').map((i:number, index:number) => (
         <div key={index} className='relative w-auto rounded-xl overflow-hidden'>
           <div className='w-[400px] h-auto max-w-full aspect-square bg-neutral-600 animate-pulse'></div>
-          <div className='h-20 p-4 w-full flex flex-col justify-between bg-neutral-800 '>
-            <p className='h-4 lg:h-5 w-24 bg-neutral-700 animate-pulse'></p>
-            <p className='h-4 lg:h-5 w-16 bg-neutral-700 animate-pulse'></p>
+          <div className='h-20 p-4 w-full flex flex-col justify-between bg-neutral-900/70 '>
+            <p className='h-4 lg:h-5 w-24 bg-neutral-600 animate-pulse'></p>
+            <p className='h-4 lg:h-5 w-16 bg-neutral-600 animate-pulse'></p>
           </div>
         </div>
       ))}
