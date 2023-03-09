103080992755efff71689da1bd393cc160b258a0
edudotdev <eduardo.er626@gmail.com>
2023-03-08 23:49:19 -0700

    feat: support for add song to next in queue

--- src/components/atoms/BtnOptionsSong/index.tsx
+++ src/components/atoms/BtnOptionsSong/index.tsx
@@ -1,6 +1,6 @@
 import { DotsThreeOutline, MusicNotesPlus, PlayCircle } from 'phosphor-react'
 import { MenuPlaylist } from '@/components/molecules/'
-import { BtnAddQueue } from '@/components/atoms'
+import { BtnAddQueue, BtnPlayNext } from '@/components/atoms'
 import { usePlayerIndexStore, usePlayerStore } from '@/store/playerStore'
 import { TRACK } from '@/types'
 
@@ -20,7 +20,6 @@ export const BtnOptionsSong = ({
   const {setTrack} = usePlayerStore()
   const {setIndex} = usePlayerIndexStore()
 
-  
   const handlePlay = () => {
     setIndex(0)
     setTrack([song])
@@ -30,19 +29,19 @@ export const BtnOptionsSong = ({
   return (
     <div className={`absolute group-hover:z-10 bottom-[88px] right-2 w-48 text-right ${className}`}>
       <div className="relative inline-block text-left">
-          <button onClick={() => setShowMenu(!showMenu)} className="p-2 bg-neutral-900/70 rounded-full focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
-            <DotsThreeOutline size={25} color="#dbeafe" weight="fill" />
-          </button>
+        <button onClick={() => setShowMenu(!showMenu)} className="p-2 bg-neutral-900/70 rounded-full focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
+          <DotsThreeOutline size={25} color="#dbeafe" weight="fill" />
+        </button>
         {showMenu && (
-          <div className="absolute right-0 bottom-[50px] mt-2 w-36 origin-bottom-right divide-y divide-gray-100 rounded-md bg-neutral-900/95 shadow-lg ring-1 ring-white ring-opacity-5 focus:outline-none">
-            <div className="py-2.5">
-              <MenuPlaylist song={song} />
-              <BtnAddQueue song={song} />
-              <button onClick={handlePlay} className='text-blue-100 group flex gap-1.5 w-full items-center py-2 px-3 transition-[padding] font-semibold text-sm hover:bg-blue-300/20'>
-                <PlayCircle size={17} color='#dbeafe' weight="fill" />
-                <span>Play</span>
-              </button>
-            </div>
+          <div className="absolute right-0 bottom-[50px] py-2.5 mt-2 w-36 rounded-md bg-neutral-900/95 shadow-lg">
+            <MenuPlaylist song={song} />
+            <hr className='opacity-25' />
+            <BtnAddQueue song={song} />
+            <button onClick={handlePlay} className='text-blue-100 group flex gap-1.5 w-full items-center py-2 px-3 font-semibold text-sm hover:bg-blue-300/20'>
+              <PlayCircle size={20} color='#dbeafe' weight="fill" />
+              <span>Play</span>
+            </button>
+            <BtnPlayNext song={song} />
           </div>
         )}
       </div>
---
+++ src/components/atoms/BtnPlayNext/index.tsx
@@ -0,0 +1,32 @@
+import { usePlayerStore, usePlayerIndexStore } from '@/store/playerStore'
+import { TRACK } from '@/types'
+import { SkipForwardCircle } from 'phosphor-react'
+import React from 'react'
+import { shallow } from 'zustand/shallow'
+
+interface BtnPlayNextProps {
+  song: TRACK
+}
+
+export const BtnPlayNext = ({
+  song
+}:BtnPlayNextProps) => {
+  const {tracks} = usePlayerStore((state) => ({
+    tracks: state.tracks
+  }), shallow)
+
+  const {index} = usePlayerIndexStore((state) => ({
+    index: state.index
+  }), shallow)
+
+  const handlePlayNext = () => {
+    tracks.splice((index + 1), 0, song)
+  }
+
+  return (
+    <button onClick={handlePlayNext} className='text-blue-100 group flex gap-1.5 w-full items-center py-2 px-3 font-semibold text-sm hover:bg-blue-300/20'>
+      <SkipForwardCircle size={20} color='#dbeafe' weight="fill" />
+      Play next
+    </button>
+  )
+}
