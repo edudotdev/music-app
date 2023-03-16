f1c78733d5055ca3d0b59c7aaae4ee83526cd27f
edudotdev <eduardo.er626@gmail.com>
2023-03-16 01:39:06 -0700

    feat: services for get playlists data

---
+++ src/services/playlists.ts
@@ -0,0 +1,22 @@
+import { PLAYLIST } from "@/types"
+import localForage from "localforage"
+
+export const getPlaylists = async () => {
+  const data = await localForage.getItem('playlists').then((result:any) => {
+    if (result === null) result = []
+    return result
+  })
+
+  return data
+}
+
+export const getPlaylistByUUID = async (uuid: string) => {
+  const data = await localForage.getItem('playlists').then((result: any) => {
+    if (result === null) result = []
+    console.log(result)
+    const playlist = result.find((playlist: PLAYLIST) => playlist.uuid === uuid)
+    return playlist
+  })
+  
+  return data
+}
