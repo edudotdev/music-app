51e4c5c25062f451a874f2360c34603fec9467bc
edudotdev <eduardo.er626@gmail.com>
2023-03-21 01:07:32 -0700

    feat: changes colors

--- src/components/organisms/Nav/index.tsx
+++ src/components/organisms/Nav/index.tsx
@@ -4,11 +4,11 @@ import { Search, NavLink } from '../../molecules/'
 import { GlobeSimple, Disc, MicrophoneStage, UsersThree, Heart, Playlist, Books } from "phosphor-react";
 
 export const Nav = () => {
-  const colorIcon: string = '#dbeafe'
-  const sizeIcon: number = 28
+  const colorIcon: string = '#fff'
+  const sizeIcon: number = 25
  
   return (
-    <div className='flex flex-col gap-8 py-10 bg-neutral-900/60 bg-opacity-50 min-w-[260px] w-[260px] h-screen sticky top-0 before:block before:absolute before:w-5 before:h-5 before:rounded-full before:left-0 before:-top-10 before:bg-blue-500 before:shadow-before-nav lg:z-30'>
+    <div className='flex flex-col gap-8 py-10 bg-neutral-900/60 bg-opacity-50 min-w-[260px] w-[260px] h-screen sticky top-0 before:block before:absolute before:w-5 before:h-5 before:rounded-full before:left-0 before:-top-10 before:bg-green-500 before:shadow-before-nav lg:z-30'>
       <Logo />
       <Search />
       <div className='flex flex-col'>
