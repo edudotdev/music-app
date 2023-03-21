bdbe3dae1ffe38b9c03f613850ddf8a5f5ab71b6
edudotdev <eduardo.er626@gmail.com>
2023-03-21 01:29:58 -0700

    feat: props for hide text and hide icon

--- src/components/atoms/BtnPlay/index.tsx
+++ src/components/atoms/BtnPlay/index.tsx
@@ -6,25 +6,31 @@ import { Play } from 'phosphor-react'
 interface BtnPlayProps {
   className?: string
   songs: TRACK[]
+  position?: number
+  showText?: boolean
+  showIcon?: boolean
 }
 
 export const BtnPlay = ({
   className,
-  songs
+  songs,
+  position,
+  showText = true,
+  showIcon = true
 }:BtnPlayProps) => {
   const {setTrack} = usePlayerStore()
   const {setIndex} = usePlayerIndexStore()
 
   const handleClick = () => {
     if(songs.length === 0) return
-    setIndex(0)
+    setIndex(position || 0)
     setTrack(songs)
   }
 
   return (
     <button onClick={handleClick} className={className}>
-      <Play size={16} color="#fff" weight="fill" />
-      Play
+      {showIcon && <Play size={16} color="#fff" weight="fill" />}
+      {showText && <span>Play</span>}
     </button>
   )
 }
