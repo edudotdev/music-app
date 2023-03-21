d657da89d1be9aa9ab5daa9822af36a6d6f91a8e
edudotdev <eduardo.er626@gmail.com>
2023-03-21 01:32:20 -0700

    feat: reduce width and text size

--- src/components/atoms/BtnOptionsSong/index.tsx
+++ src/components/atoms/BtnOptionsSong/index.tsx
@@ -1,6 +1,6 @@
 import { DotsThreeOutline } from 'phosphor-react'
 import { MenuPlaylist } from '@/components/molecules/'
-import { BtnAddQueue, BtnPlay, BtnPlayNext } from '@/components/atoms'
+import { BtnPlayLast, BtnPlay, BtnPlayNext } from '@/components/atoms'
 import { TRACK } from '@/types'
 
 interface BtnOptionsSongProps {
@@ -18,18 +18,18 @@ export const BtnOptionsSong = ({
 }:BtnOptionsSongProps) => {  
 
   return (
-    <div className={`absolute group-hover:z-10 bottom-[88px] right-2 w-48 text-right ${className}`}>
+    <div className={`absolute group-hover:z-10 bottom-[68px] right-2 text-right ${className}`}>
       <div className="relative inline-block text-left">
         <button onClick={() => setShowMenu(!showMenu)} className="p-2 bg-neutral-900/70 rounded-full focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
-          <DotsThreeOutline size={25} color="#dbeafe" weight="fill" />
+          <DotsThreeOutline size={25} color="#fff" weight="fill" />
         </button>
         {showMenu && (
-          <div className="absolute right-0 bottom-[50px] py-2.5 mt-2 w-36 rounded-md bg-neutral-900/95 shadow-lg">
+          <div className="absolute right-0 bottom-[50px] py-2.5 mt-2 w-28 rounded-md bg-neutral-900/95 shadow-lg">
             <MenuPlaylist song={song} />
             <hr className='opacity-25' />
-            <BtnAddQueue song={song} />
-            <BtnPlay songs={[song]} className='text-blue-100 group flex gap-1.5 w-full items-center py-2 px-3 font-semibold text-sm hover:bg-blue-300/20' />
+            <BtnPlay songs={[song]} showIcon={false} className='text-white group flex w-full items-center py-1.5 px-2 text-xs font-semibold hover:bg-neutral-400/10' />
             <BtnPlayNext song={song} />
+            <BtnPlayLast song={song} />
           </div>
         )}
       </div>
