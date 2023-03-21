e0a6e814043b5ed9359f5c7272cf5d9c5146e4c3
edudotdev <eduardo.er626@gmail.com>
2023-03-21 01:17:34 -0700

    feat: reduce text size

--- src/components/molecules/CardSong/index.tsx
+++ src/components/molecules/CardSong/index.tsx
@@ -33,12 +33,12 @@ export const CardSong = ({
       <BtnOptionsSong song={{id,music,title, artist,image}} setShowMenu={setShowMenu} showMenu={showMenu} className='opacity-0 group-hover:opacity-100 transition-all' />
       <div onClick={handlePlay} className='relative cursor-pointer rounded-xl overflow-hidden active:scale-[.99] transition-transform'>
         <Image src={image} width={400} height={400} className='w-full' alt={title} />
-        <div className='h-20 relative overflow-hidden rounded-b-xl'>
+        <div className='h-14 relative overflow-hidden rounded-b-xl'>
           <Image src={image} width={400} height={400} className='absolute h-[900px] saturate-150 -bottom-9 blur-2xl' quality={1} alt={title} />
         </div>
-        <div className='absolute bottom-0 p-4 flex flex-col justify-between bg-neutral-800 bg-opacity-25 w-full left-1/2 -translate-x-1/2 h-20 backdrop-blur-md'>
-          <p className='text-white/90 font-bold text-base lg:text-lg truncate'>{title}</p>
-          <p className='text-white/75 text-xs lg:text-sm font-semibold truncate'>{artist}</p>
+        <div className='absolute bottom-0 px-4 py-2.5 flex flex-col justify-between bg-neutral-800 bg-opacity-25 w-full left-1/2 -translate-x-1/2 h-14 backdrop-blur-md'>
+          <p className='text-white/90 font-bold text-sm truncate'>{title}</p>
+          <p className='text-white/75 text-xs font-semibold truncate'>{artist}</p>
         </div>
       </div>
     </div>
