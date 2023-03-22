e4e2394f451517be3167969dfe38a2be96c29748
edudotdev <eduardo.er626@gmail.com>
2023-03-22 02:15:26 -0700

    feat: change colors

--- src/components/molecules/CardQueue/index.tsx
+++ src/components/molecules/CardQueue/index.tsx
@@ -19,22 +19,22 @@ export const CardQueue = () => {
   return (
     <div className='bg-neutral-900 border-2 border-neutral-800/90 py-2 shadow-xl rounded-lg grid max-h-72 overflow-y-auto'>
       {tracks[0].id.length > 0 && tracks?.map((song: TRACK, index: number) =>  (
-        <button key={index} onClick={() => setIndex(index)} className='relative w-[340px] gap-3 text-start flex justify-between hover:bg-neutral-700/90 p-3'>
+        <button key={index} onClick={() => setIndex(index)} className='relative w-[350px] gap-3 text-start flex justify-between hover:bg-neutral-700/90 p-3'>
           <div className='flex gap-2 h-full items-center'>
             <Image src={song.image} width={45} height={45} alt={song.title} className='rounded-md' />
             <div className='flex flex-col'>
-              <span className='text-sm'>{song.title}</span>
+              <span className={`${indexS === index ? 'text-green-500' : 'text-white'} text-sm`}>{song.title}</span>
               <span className='text-xs text-neutral-400'>{song.artist}</span>
             </div>
           </div>
           {indexS === index && (
-            <div className='absolute h-2 w-2 right-3 self-center bg-blue-500 rounded-full'></div>
+            <div className='absolute h-2 w-2 right-3 self-center bg-green-500 rounded-full'></div>
           )}
         </button>
       ))}
       {tracks[0].id.length === 0 && (
         <div className='w-80 flex items-center gap-3 px-2'>
-          <div className='bg-neutral-800 rounded-lg w-[45px] min-w-45px] aspect-square grid place-content-center'>
+          <div className='bg-neutral-800 rounded-lg w-[45px] min-w-[45px] aspect-square grid place-content-center'>
             <MusicNotes size={20} color="#aaa" weight="fill" />
           </div>
           <span className='text-neutral-300'>No playing music</span>
