aaa7e5aa69dcb262978a939cb8c6d9b8dcdac6fc
edudotdev <eduardo.er626@gmail.com>
2023-03-22 02:17:06 -0700

    feat: list songs

--- src/pages/for-you.tsx
+++ src/pages/for-you.tsx
@@ -1,11 +1,40 @@
 import { Layout } from '@/components/Layout'
+import { GetServerSideProps, InferGetServerSidePropsType, NextPage } from 'next'
 import React from 'react'
+import { forYou } from '@/services/forYou'
+import { getCountryCode } from '@/services/getCountryCode'
+import { TableTopSongs } from '@/components/organisms/TableTopSongs'
 
-export default function ForYou () {
+type Props = InferGetServerSidePropsType<typeof getServerSideProps>
+
+const ForYou: NextPage<Props> = ({songs, code}) => {
+
   return (
     <Layout title='For You'>
-      <h2 className='text-neutral-100 font-bold text-4xl'>For You</h2>
+      <header className='flex flex-col gap-2 items-start'>
+        <h2 className='text-neutral-100 font-bold text-4xl flex gap-3 relative'>Trending <p className='text-green-400 absolute text-lg -right-8'>{code}</p></h2>
+        <span className='text-base font-semibold text-neutral-400'>Songs #1-50</span>
+      </header>
+     <TableTopSongs songs={songs.songs} />
     </Layout>
   )
 }
 
+export default ForYou
+
+export const getServerSideProps:GetServerSideProps = async () => {
+
+  const code = await getCountryCode()
+
+  try {
+    return {
+      props: {
+        songs: await forYou(code),
+        code: code
+      }
+    }    
+  } 
+  catch (_error) {
+    return { redirect: { destination: '/404', permanent: false } }
+  }
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,36 +1,19 @@
 import { Layout } from '@/components/Layout'
 import { GetServerSideProps, InferGetServerSidePropsType, NextPage } from 'next'
 import { discover } from '@/services/discover'
-import { CardTopSong } from '@/components/molecules'
-import { TRACK } from '@/types'
+import { TableTopSongs } from '@/components/organisms/TableTopSongs'
 
 type Props = InferGetServerSidePropsType<typeof getServerSideProps>
 
 const  Home: NextPage<Props> = (props) => {
 
-
   return (
     <Layout title='Home'>
-      <h2 className='text-neutral-100 font-bold text-4xl'>Discover Music</h2>
-      <div className='w-full h-96 rounded-3xl bg-gradient-to-r from-blue-800 to-blue-600'></div>
-
-      <h2 className='text-neutral-100 font-bold text-4xl flex flex-col gap-3'><span className='text-base text-neutral-400'>Top Global</span> Songs #1-50</h2>
-
-      <div className='flex flex-col gap-4'>
-        <div className='flex items-center py-4 px-6 justify-between gap-10 text-white font-bold uppercase'>
-          <div className='flex gap-8'>
-            <div className='w-5'>#</div>
-            <div className='w-[80px]'></div>
-            <div className='w-[200px] max-w-[200px]'>Name Song</div>
-          </div>
-          <div className='w-[160px] max-w-[160px]'>Artist</div>
-          <div className='w-[41px]'>Like</div>
-          <div className='w-[40px]'>Play</div>
-        </div>
-        {props.discover.map((song:TRACK, index:number) => (
-          <CardTopSong key={song.id} song={song} index={index}  />
-        ))}
-      </div>
+      <header className='flex flex-col gap-2'>
+        <h2 className='text-neutral-100 font-bold text-4xl flex gap-3'>Top Global</h2>
+        <span className='text-base font-semibold text-neutral-400'>Songs #1-50</span>
+      </header>
+      <TableTopSongs songs={props.discover} />
     </Layout>
   )
 }
