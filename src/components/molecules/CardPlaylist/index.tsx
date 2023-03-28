4f66022f4e471f5b138beb0c66f7be0c16c44ffb
edudotdev <eduardo.er626@gmail.com>
2023-03-27 21:11:48 -0700

    removed btn option playlist

--- src/components/molecules/CardPlaylist/index.tsx
+++ src/components/molecules/CardPlaylist/index.tsx
@@ -1,7 +1,6 @@
-import React, { useState } from 'react'
 import Link from 'next/link'
 import { PLAYLIST } from '@/types'
-import { BtnOptionsPlaylist, BtnPlay } from '@/components/atoms'
+import { BtnPlay } from '@/components/atoms'
 import { ThumbnailPlaylist } from '@/components/molecules'
 
 interface CardPlaylistProps {
@@ -11,11 +10,8 @@ interface CardPlaylistProps {
 export const CardPlaylist = ({
   playlist
 }:CardPlaylistProps) => {
-  const [showMenu, setShowMenu] = useState(false)
-
   return (
-    <div key={playlist.uuid} className='relative flex flex-col w-auto aspect-square group hover:opacity-90' onMouseLeave={() => setShowMenu(false)}>
-      {/* <BtnOptionsPlaylist playlist={playlist} showMenu={showMenu} setShowMenu={setShowMenu} className='bottom-12 right-2 opacity-0 group-hover:opacity-100 transition-opacity' /> */}
+    <div key={playlist.uuid} className='relative flex flex-col w-auto aspect-square group hover:opacity-90'>
       <BtnPlay songs={playlist.song} className='absolute bg-green-500 p-4 rounded-full shadow-xl bottom-14 right-4 opacity-0 group-hover:opacity-100 transition-opacity' showText={false} />
       <div className='h-full rounded-xl overflow-hidden border-2 border-neutral-700 border-opacity-70'>
         <Link href={`/playlist/${playlist.uuid}`} className='active:scale-[.99]'>
