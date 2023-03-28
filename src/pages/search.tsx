28b04176c02bcb73a254352fab39f1a4c068bd0a
edudotdev <eduardo.er626@gmail.com>
2023-03-27 21:00:16 -0700

    feat: send new props to cardSong and update grid

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -3,10 +3,10 @@ import { NextPage } from 'next'
 import { Layout } from '@/components/Layout'
 import { CardSong } from '@/components/molecules/CardSong'
 import { SkeletonSearch } from '@/components/skeletons/SkeletonSearch'
-import { MagnifyingGlass } from "phosphor-react";
 import Router from 'next/router'
 import useSWR from 'swr'
 import axios from 'axios'
+import { TRACK } from '@/types'
 
 const fetcher = (url:any) => axios.get(url).then(res => res.data)
 
@@ -15,21 +15,18 @@ const Search: NextPage = () => {
 
   const { data, error, isLoading } = useSWR(`/api/search?song=${router.query.song}`, fetcher)
 
-  if (error) {
-    Router.push('/404')
-  }
+  if (error) Router.push('/404')
 
   return (
     <Layout title={`${router.query.song} - search`}>
-      <h2 className='flex gap-5 text-neutral-100 font-bold text-4xl'>
-        <MagnifyingGlass size={40} color="#dbeafe" weight="fill" />
-        <span className='text-blue-100'>Search - <span className='text-blue-300'>{router.query.song}</span></span>
+      <h2 className='flex justify-center lg:justify-start gap-5 text-neutral-100 font-bold text-2xl lg:text-4xl'>
+        <span className='text-green-400'>Search - <span className='text-white'>{router.query.song}</span></span>
       </h2>
-      <div className='grid gap-10 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4'>
-        {isLoading&&<SkeletonSearch />}
-        {data&&data.songs?.map((song:any) => ((
+      <div className='grid gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5'>
+        {isLoading && <SkeletonSearch />}
+        {data&&data.songs?.map((song: TRACK, index:number) => ((
           <div key={song.id}>
-            <CardSong song={song} />
+            <CardSong songs={data.songs} position={index} />
           </div>
         )))}
       </div>
