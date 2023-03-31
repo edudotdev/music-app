a538740e683eca3a0ecf00d893d8f98bb0f82f46
edudotdev <eduardo.er626@gmail.com>
2023-03-31 03:15:11 -0700

    reduce text and icon size

--- src/components/atoms/Logo/index.tsx
+++ src/components/atoms/Logo/index.tsx
@@ -4,8 +4,8 @@ import { SpotifyLogo } from 'phosphor-react'
 
 export const Logo = () => {
   return (
-    <h1 className='flex gap-2 text-4xl font-bold text-white justify-center opacity-90'>
-      <SpotifyLogo size={38} color="#fff" weight="fill" />
+    <h1 className='flex gap-2 text-3xl font-bold text-white justify-center opacity-90'>
+      <SpotifyLogo size={36} color="#fff" weight="fill" />
       <span>Musikc</span>
     </h1>
   )
