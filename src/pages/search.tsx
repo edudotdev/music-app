b540aaf0ad5ba8f9cdbcfb9de0d2a24a2e94e26f
edudotdev <eduardo.er626@gmail.com>
2023-03-31 03:18:37 -0700

    feat: search page

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -7,31 +7,40 @@ import Router from 'next/router'
 import useSWR from 'swr'
 import axios from 'axios'
 import { TRACK } from '@/types'
+import { Search } from '@/components/molecules'
 
-const fetcher = (url:any) => axios.get(url).then(res => res.data)
+const fetcher = (url:string) => axios.get(url).then(res => res.data)
 
-const Search: NextPage = () => {
+const SearchPage: NextPage = () => {
   const router = useRouter()
 
   const { data, error, isLoading } = useSWR(`/api/search?song=${router.query.song}`, fetcher)
 
   if (error) Router.push('/404')
 
+  console.log(router.query.song)
+
   return (
-    <Layout title={`${router.query.song} - search`}>
-      <h2 className='flex justify-center lg:justify-start gap-5 text-neutral-100 font-bold text-2xl lg:text-4xl'>
-        <span className='text-green-400'>Search - <span className='text-white'>{router.query.song}</span></span>
-      </h2>
-      <div className='grid gap-3 sm:gap-6 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5'>
-        {isLoading && <SkeletonSearch />}
-        {data&&data.songs?.map((song: TRACK, index:number) => ((
-          <div key={song.id}>
-            <CardSong songs={[...data.songs]} position={index} />
+    <Layout title={`${router.query.song === undefined ?  'Search Page' : router.query.song} - search`}>
+      
+      <Search />
+      {router.query.song !== undefined && 
+        <>
+          <h2 className='flex justify-center lg:justify-start gap-5 text-neutral-100 font-bold text-2xl lg:text-4xl'>
+            <span className='text-green-400'>Search - {router.query.song}</span>
+          </h2>
+          <div className='grid gap-3 sm:gap-6 grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5'>
+            {isLoading && <SkeletonSearch />}
+            {data&&data.songs?.map((song: TRACK, index:number) => ((
+              <div key={song.id}>
+                <CardSong songs={[...data.songs]} position={index} />
+              </div>
+            )))}
           </div>
-        )))}
-      </div>
+        </>
+      }
     </Layout>
   )
 }
 
-export default Search
+export default SearchPage
