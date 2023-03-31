bc2a8d2803ebaac9531c63d8c5905baab6051235
edudotdev <eduardo.er626@gmail.com>
2023-03-31 03:12:59 -0700

    hidde tooltip in mobile

--- src/components/molecules/Tooltip/index.tsx
+++ src/components/molecules/Tooltip/index.tsx
@@ -13,7 +13,7 @@ export const Tooltip = ({
 }:TooltipProps) => {
   return (
     <div className='relative group grid place-items-center'>
-      <span className='absolute group-hover:visible group-hover:opacity-100 opacity-0 invisible transition-opacity delay-300 text-xs font-bold bg-neutral-800 text-white px-2 py-1.5 rounded-sm -top-full -translate-y-4 left-1/2 -translate-x-1/2 shadow-lg whitespace-nowrap'>{text}</span>
+      <span className='absolute hidden lg:block group-hover:visible group-hover:opacity-100 opacity-0 invisible transition-opacity delay-300 text-xs font-bold bg-neutral-800 text-white px-2 py-1.5 rounded-sm -top-full -translate-y-4 left-1/2 -translate-x-1/2 shadow-lg whitespace-nowrap'>{text}</span>
       {children}
     </div>
   )
