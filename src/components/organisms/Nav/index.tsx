3e97c629df57d505dce799a879e9374b7d01c6e5
edudotdev <eduardo.er626@gmail.com>
2023-03-31 03:12:35 -0700

    feat: btn search

--- src/components/organisms/Nav/index.tsx
+++ src/components/organisms/Nav/index.tsx
@@ -1,31 +1,35 @@
 import React from 'react'
 import { Logo } from '../../atoms/'
 import { Search, NavLink } from '../../molecules/'
-import { GlobeSimple, Disc, MicrophoneStage, UsersThree, Heart, Playlist, Books } from "phosphor-react";
+import { GlobeSimple, Disc, MagnifyingGlass, Heart, Playlist, Books } from "phosphor-react";
 
 export const Nav = () => {
   const colorIcon: string = '#fff'
   const sizeIcon: number = 25
  
   return (
-    <div className='flex flex-col gap-8 py-10 bg-neutral-900/60 bg-opacity-50 min-w-[260px] w-[260px] h-screen sticky top-0 before:block before:absolute before:w-5 before:h-5 before:rounded-full before:left-0 before:-top-10 before:bg-green-500 before:shadow-before-nav lg:z-30'>
+    <div className='hidden md:flex flex-col gap-8 py-10 bg-neutral-900/60 bg-opacity-50 min-w-[200px] w-[280px] h-screen sticky top-0 before:block before:absolute before:w-5 before:h-5 before:rounded-full before:left-0 before:-top-10 before:bg-green-500 before:shadow-before-nav lg:z-30'>
       <Logo />
-      <Search />
+      {/* <Search /> */}
+      <NavLink variant='desktop' to='/search' text='Search'>
+          <MagnifyingGlass size={sizeIcon} color={colorIcon} weight="fill" />
+        </NavLink>
       <div className='flex flex-col'>
+        
         <h2 className='text-neutral-400 ml-6 pb-2 text-sm'>Explore</h2>
-        <NavLink to='/' text='Discover'>
+        <NavLink variant='desktop' to='/' text='Discover'>
           <GlobeSimple size={sizeIcon} color={colorIcon} weight="fill" />
         </NavLink>
-        <NavLink to='/for-you' text='For you'>
+        <NavLink variant='desktop' to='/for-you' text='For you'>
           <Disc size={sizeIcon} color={colorIcon} weight="fill" />
         </NavLink>
       </div>
       <div className='flex flex-col'>
         <h2 className='text-neutral-400 ml-6 pb-2 text-sm'>Your collection</h2>
-        <NavLink to='/playlists' text='Playlists'>
+        <NavLink variant='desktop' to='/playlists' text='Playlists'>
           <Playlist size={sizeIcon} color={colorIcon} weight="fill" />
         </NavLink>
-        <NavLink to='/likes' text='Likes'>
+        <NavLink variant='desktop' to='/likes' text='Likes'>
           <Heart size={sizeIcon} color={colorIcon} weight="fill" />
         </NavLink>
       </div>
