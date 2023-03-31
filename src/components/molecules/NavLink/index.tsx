4f6e698de9d4b18fa66f4a0bf0ea3a3737448264
edudotdev <eduardo.er626@gmail.com>
2023-03-31 03:13:45 -0700

    feat: variant mobile

--- src/components/molecules/NavLink/index.tsx
+++ src/components/molecules/NavLink/index.tsx
@@ -4,26 +4,37 @@ import Link from 'next/link'
 interface NavLinkProps {
   children: React.ReactElement
   to: string
-  text:string
+  text?:string
+  variant: 'desktop' | 'mobile'
 }
 
 export const NavLink = ({
   children,
   to,
-  text
+  text,
+  variant
 }:NavLinkProps) => {
   const router = useRouter()
   
   const styleDefault = 'font-semibold pl-6 py-4 text-sm flex gap-4 items-center hover:bg-neutral-600/20'
+  const styleDefaultMobile = 'py-2 flex gap-2 items-center text-green-400 text-xs font-bold'
+
   const styles = 'text-neutral-200'
+
   const styleActive = 'relative text-white overflow-hidden before:absolute before:w-1 before:h-6 before:right-0 before:rounded-l-lg before:top-1/2 before:-translate-y-1/2 before:bg-green-500'
 
-  return (
+  return variant === 'desktop' ? (
     <Link 
-      className={`${styleDefault} ${styles} ${router.asPath === to? styleActive : styles }`} 
+      className={`${styleDefault}  ${router.asPath === to? styleActive : styles }`} 
       href={to}>
         {children}
         <span>{text}</span>
     </Link>
-  )
+  ) :  
+  <Link 
+    className={`${styleDefaultMobile}`} 
+    href={to}>
+      {children}
+      {router.asPath === to? <span>{text}</span> : null }
+  </Link>
 }
