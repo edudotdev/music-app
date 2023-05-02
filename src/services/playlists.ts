7b5a60740b5108ebe4f15410bf7f84e82d7689f7
edudotdev <eduardo.er626@gmail.com>
2023-05-01 21:23:34 -0700

    feat: change variable name

--- src/services/playlists.ts
+++ src/services/playlists.ts
@@ -2,20 +2,20 @@ import { PLAYLIST } from "@/types"
 import localForage from "localforage"
 
 export const getPlaylists = async () => {
-  const data = await localForage.getItem('playlists').then((result:any) => {
+  const playlist = await localForage.getItem('playlists').then((result:any) => {
     if (result === null) result = []
     return result
   })
 
-  return data
+  return playlist
 }
 
 export const getPlaylistByUUID = async (uuid: string) => {
-  const data = await localForage.getItem('playlists').then((result: any) => {
+  const playlist = await localForage.getItem('playlists').then((result: any) => {
     if (result === null) result = []
     const playlist = result.find((playlist: PLAYLIST) => playlist.uuid === uuid)
     return playlist
   })
   
-  return data
+  return playlist
 }
