4d387342448041ee4a1db496f4e99ba8844212e3
edudotdev <eduardo.er626@gmail.com>
2023-05-02 02:03:17 -0700

    feat: hook for get current time song

---
+++ src/hooks/useCurrentTime.tsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+
+export const useCurrentTime = () => {
+  const [currentTime, setCurrentTime] = useState(0)
+
+  const handleTimeUpdate = (event: React.SyntheticEvent<HTMLAudioElement>) => {
+    const target = event.target as HTMLAudioElement;
+    const newCurrentTime = target.currentTime;
+    setCurrentTime(newCurrentTime);
+  }
+
+  return {currentTime, setCurrentTime, handleTimeUpdate}
+}
