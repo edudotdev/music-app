9926a476a3ed45019594806a8d450f7beec1b687
edudotdev <eduardo.er626@gmail.com>
2023-05-02 01:29:22 -0700

    feat: list all playlists

--- src/components/organisms/Nav/index.tsx
+++ src/components/organisms/Nav/index.tsx
@@ -1,7 +1,6 @@
-import React from 'react'
-import { Logo } from '../../atoms/'
-import { Search, NavLink } from '../../molecules/'
-import { GlobeSimple, Disc, MagnifyingGlass, Heart, Playlist, Books } from "phosphor-react";
+import { Logo } from '@/components/atoms'
+import { NavLink, ListPlaylists } from '@/components/molecules'
+import { GlobeSimple, Disc, MagnifyingGlass, Heart, Books } from "phosphor-react";
 
 export const Nav = () => {
   const colorIcon: string = '#fff'
@@ -10,12 +9,10 @@ export const Nav = () => {
   return (
     <div className='hidden md:flex flex-col gap-8 py-10 bg-neutral-900/60 bg-opacity-50 min-w-[200px] w-[280px] h-screen sticky top-0 before:block before:absolute before:w-5 before:h-5 before:rounded-full before:left-0 before:-top-10 before:bg-green-500 before:shadow-before-nav lg:z-30'>
       <Logo />
-      {/* <Search /> */}
       <NavLink variant='desktop' to='/search' text='Search'>
           <MagnifyingGlass size={sizeIcon} color={colorIcon} weight="fill" />
         </NavLink>
       <div className='flex flex-col'>
-        
         <h2 className='text-neutral-400 ml-6 pb-2 text-sm'>Explore</h2>
         <NavLink variant='desktop' to='/' text='Discover'>
           <GlobeSimple size={sizeIcon} color={colorIcon} weight="fill" />
@@ -27,16 +24,14 @@ export const Nav = () => {
       <div className='flex flex-col'>
         <h2 className='text-neutral-400 ml-6 pb-2 text-sm'>Your collection</h2>
         <NavLink variant='desktop' to='/playlists' text='Playlists'>
-          <Playlist size={sizeIcon} color={colorIcon} weight="fill" />
+          <Books size={sizeIcon} color={colorIcon} weight="fill" />
         </NavLink>
         <NavLink variant='desktop' to='/likes' text='Likes'>
           <Heart size={sizeIcon} color={colorIcon} weight="fill" />
         </NavLink>
       </div>
       <hr className='opacity-30 mx-5' />
-      <div className='flex flex-col'>
-        
-      </div>
+      <ListPlaylists />
     </div>
   )
 }
