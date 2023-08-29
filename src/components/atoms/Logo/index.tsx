714a162aed3d5fd008cf73e1f8c4449a392f7757
EduardoRL <eduardo.er626@gmail.com>
2023-08-28 17:07:05 -0700

    feat: new name and icon

--- src/components/atoms/Logo/index.tsx
+++ src/components/atoms/Logo/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import { SpotifyLogo } from 'phosphor-react'
+import { WaveTriangle  } from 'phosphor-react'
 
 
 export const Logo = () => {
   return (
     <h1 className='flex gap-2 text-3xl font-bold text-white justify-center opacity-90'>
-      <SpotifyLogo size={36} color="#fff" weight="fill" />
-      <span>Musikc</span>
+      <WaveTriangle  size={36} color="#fff" weight="duotone" />
+      <span>Zounds</span>
     </h1>
   )
 }
