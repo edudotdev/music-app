c55f5b4c8a1cdfa92f500af69b3e121151f16779
EduardoRL <eduardo.er626@gmail.com>
2023-12-13 01:39:04 -0700

    feat: btnSearch component

--- src/components/organisms/Nav/index.tsx
+++ src/components/organisms/Nav/index.tsx
@@ -1,4 +1,4 @@
-import { Logo } from '@/components/atoms'
+import { Logo, BtnSearch } from '@/components/atoms'
 import { NavLink, ListPlaylists } from '@/components/molecules'
 import { GlobeSimple, Disc, MagnifyingGlass, Heart, Books } from "phosphor-react";
 
@@ -9,9 +9,7 @@ export const Nav = () => {
   return (
     <div className='hidden md:flex flex-col gap-8 py-10 bg-neutral-900/60 bg-opacity-50 min-w-[200px] w-[280px] h-screen sticky top-0 before:block before:absolute before:w-5 before:h-5 before:rounded-full before:left-0 before:-top-10 before:bg-green-500 before:shadow-before-nav lg:z-30'>
       <Logo />
-      <NavLink variant='desktop' to='/search' text='Search'>
-          <MagnifyingGlass size={sizeIcon} color={colorIcon} weight="fill" />
-        </NavLink>
+      <BtnSearch />
       <div className='flex flex-col'>
         <h2 className='text-neutral-400 ml-6 pb-2 text-sm'>Explore</h2>
         <NavLink variant='desktop' to='/' text='Discover'>
