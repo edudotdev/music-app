3f19bd8b448e3bf95b05fabf18ed6db26cadec71
edudotdev <eduardo.er626@gmail.com>
2024-04-10 17:46:49 -0700

    fix: mobile ui

--- src/components/molecules/CardSong/index.tsx
+++ src/components/molecules/CardSong/index.tsx
@@ -16,14 +16,14 @@ export const CardSong = ({
   const { id, music, title, artist, image } = songs[position]
 
   return (
-    <div className='relative group/play flex items-center gap-1 hover:bg-neutral-600/40 odd:bg-neutral-700/30 px-2'>
+    <div className='relative group/play flex items-center gap-1 hover:bg-neutral-600/40 odd:bg-neutral-700/30 px-1'>
       <BtnLike song={{id, music, title, artist, image}} className='!bg-transparent' />
-      <div className='relative rounded-md overflow-hidden'>
+      <div className='relative rounded-md overflow-hidden min-w-[45px]'>
         <img src={image} width={45} height={45} className='' alt={title} />
         <BtnPlay songs={songs} position={position} className='absolute grid place-content-center z-10 bg-black/40 p-2.5 inset-0 opacity-0 group-hover/play:opacity-100' showText={false} />
       </div>
-      <Link href={`/track/${id}`} className='group/text'>
-        <div className='bottom-0 px-2 py-2.5 flex flex-col justify-between'>
+      <Link href={`/track/${id}`} className='group/text overflow-hidden'>
+        <div className='bottom-0 px-2 py-2.5 flex flex-col truncate justify-between'>
           <p className='text-white/90 font-bold text-sm truncate group-hover/text:underline'>{title}</p>
           <p className='text-white/75 text-xs font-semibold truncate'>{artist}</p>
         </div>
