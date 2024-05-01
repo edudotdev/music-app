b60e721ebfe49a1a981176c8656953754c9cfe25
edudotdev <eduardo.er626@gmail.com>
2024-04-30 20:06:12 -0700

    pending solution: unavailable charts API

--- src/pages/for-you.tsx
+++ src/pages/for-you.tsx
@@ -9,6 +9,7 @@ import axios from 'axios'
 import { useRouter } from 'next/router'
 import { useModalPlaylist } from '@/store/playlistsStore'
 import { ModalNewPlaylist } from '@/components/molecules'
+import Link from 'next/link'
 
 const fetcher = (url: string) => axios.get(url).then(res => res.data)
 
@@ -31,15 +32,22 @@ const ForYou: NextPage = () => {
         <div className='flex flex-col'>
           <h2 className='text-neutral-100 font-bold text-4xl flex gap-3 relative'>
             Trending 
-            {!isLoading && <p className='text-green-400 absolute text-lg -right-8'>{data.code}</p>}
+            {/* {!isLoading && <p className='text-green-400 absolute text-lg -right-8'>{data.code}</p>} */}
           </h2>
           <span className='text-base font-semibold text-neutral-400'>Songs #1-50</span>
         </div>
-        {!isLoading && <BtnPlay songs={[...data.songs.songs]} className='flex items-center justify-center gap-2 bg-green-600 py-2 w-32 text-white text-sm font-semibold rounded-md' />}
+        {/* {!isLoading && <BtnPlay songs={[...data.songs.songs]} className='flex items-center justify-center gap-2 bg-green-600 py-2 w-32 text-white text-sm font-semibold rounded-md' />} */}
       </header>
-    {isLoading && <SkeletonTableSongs />}
+
+      <div className='flex justify-center flex-col items-center gap-6'>
+        <h2 className='text-2xl lg:text-4xl font-bold text-white'>Music charts not available</h2>
+        <Link href='/search' className='bg-white text-neutral-700 font-semibold rounded-full py-2.5 px-5 self-center'>
+          Find music
+        </Link>
+      </div>
+    {/* {isLoading && <SkeletonTableSongs />}
     {!isLoading && <TableTopSongs songs={[...data.songs.songs]} />}
-    {showModal && <ModalNewPlaylist setShowModal={setShowModal} song={song} />}
+    {showModal && <ModalNewPlaylist setShowModal={setShowModal} song={song} />} */}
     </Layout>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,12 +7,13 @@ import { useRouter } from 'next/router'
 import { SkeletonTableSongs } from '@/components/skeletons/SkeletonTableSongs'
 import { useModalPlaylist } from '@/store/playlistsStore'
 import { ModalNewPlaylist } from '@/components/molecules'
+import Link from 'next/link'
 
 const fetcher = (url: string) => axios.get(url).then(res => res.data)
 
 const Home = () => {
   const router = useRouter()
-
+  
   const { data, error, isLoading } = useSWR(`/api/discover`, fetcher)
 
   if (error) router.push('/404')
@@ -30,11 +31,19 @@ const Home = () => {
           <h2 className='text-neutral-100 font-bold text-4xl flex gap-3'>Top Global</h2>
           <span className='text-base font-semibold text-neutral-400'>Songs #1-50</span>
         </div>
-        {!isLoading && <BtnPlay songs={[...data?.discover]} className='flex items-center justify-center gap-2 bg-green-600 py-2 w-32 text-white text-sm font-semibold rounded-md' />}
+        {/* {!isLoading && <BtnPlay songs={[...data?.discover]} className='flex items-center justify-center gap-2 bg-green-600 py-2 w-32 text-white text-sm font-semibold rounded-md' />} */}
       </header>
-      {isLoading && <SkeletonTableSongs />}
+
+      <div className='flex justify-center flex-col items-center gap-6'>
+        <h2 className='text-2xl lg:text-4xl font-bold text-white'>Music charts not available</h2>
+        <Link href='/search' className='bg-white text-neutral-700 font-semibold rounded-full py-2.5 px-5 self-center'>
+          Find music
+        </Link>
+      </div>
+
+      {/* {isLoading && <SkeletonTableSongs />}
       {!isLoading && <TableTopSongs songs={[...data?.discover]} />}
-      {showModal && <ModalNewPlaylist setShowModal={setShowModal} song={song} />}
+      {showModal && <ModalNewPlaylist setShowModal={setShowModal} song={song} />} */}
     </Layout>
   )
 }
