c91c29671144dcd7d86b88d9d21e03487cd55474
edudotdev <eduardo.er626@gmail.com>
2024-04-30 19:15:02 -0700

    fix: key prop

--- src/components/skeletons/SkeletonSearch.tsx
+++ src/components/skeletons/SkeletonSearch.tsx
@@ -4,18 +4,16 @@ export const SkeletonSearch = () => {
 
   return (
     <div className='flex flex-col'>
-      {Array(5).fill('').map((index:number) => (
-          <div key={index} className='flex items-center gap-1 relative w-auto overflow-hidden h-[56px] p-1 bg-neutral-600 animate-pulse'>
-            <Heart size={25} color="#ccc" weight="fill" className='w-[41px]' />
-            
-            <div className='min-w-[45px] h-[45px] rounded-md bg-neutral-900/70'>
-            </div>
-            <div className='flex flex-col gap-1 justify-center h-[45px] w-full'>
-              <p className='ml-1 h-3 w-44 bg-neutral-900/70'></p>
-              <p className='ml-1 h-3 w-28 bg-neutral-900/70'></p>
-            </div>
+      {Array(5).fill('').map((e: any, index:number) => (
+        <div key={index} className='flex items-center gap-1 relative w-auto overflow-hidden h-[56px] p-1 bg-neutral-600 animate-pulse'>
+          <Heart size={25} color="#ccc" weight="fill" className='w-[41px]' />
+          <div className='min-w-[45px] h-[45px] rounded-md bg-neutral-900/70'></div>
+          <div className='flex flex-col gap-1 justify-center h-[45px] w-full'>
+            <p className='ml-1 h-3 w-44 bg-neutral-900/70'></p>
+            <p className='ml-1 h-3 w-28 bg-neutral-900/70'></p>
           </div>
-        ))}
+        </div>
+      ))}
     </div>
   )
 }
--- src/components/skeletons/SkeletonTableSongs.tsx
+++ src/components/skeletons/SkeletonTableSongs.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const SkeletonTableSongs = () => {
   return (
     <div className='flex flex-col lg:mt-16'>
-      {Array(20).fill('').map((i:number, index:number) => (
+      {Array(20).fill('').map((e: any, index: number) => (
         <div key={index} className='w-full h-[61px] odd:bg-neutral-700/30 odd:animate-pulse'></div>
       ))}
     </div>
